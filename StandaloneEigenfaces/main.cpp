0eeed63842551cf33ecedb22592bba170f23ae29
Hayden Waisanen <haydenzone@gmail.com>
2013-04-19 09:18:54 -0600

    Modularised eigenface functionality

--- StandaloneEigenfaces/main.cpp
+++ StandaloneEigenfaces/main.cpp
@@ -2,15 +2,34 @@
 #include "mainwindow.h"
 #include "matrix.h"
 #include "iostream"
-
+#include<dirent.h>
+int eigenface_match( Image faces[], const int IMAGE_COUNT );
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     MainWindow w2;
-    w.show();
-    w2.show();
+    QImage m;
+    int min_face_i;
     const int IMAGE_COUNT = 5;
+
+    /*
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir ("/home/student/1905008/IP/faces94/female/training")) != NULL) {
+      while ((ent = readdir (dir)) != NULL) {
+          if(ent->d_name[0] == '.')
+              continue;
+        printf ("%s\n", ent->d_name);
+      }
+      closedir (dir);
+    } else {
+      perror ("");
+      return EXIT_FAILURE;
+    }
+    return 0;
+*/
+
 
     Image mytest[IMAGE_COUNT];
 
@@ -22,31 +41,50 @@ int main(int argc, char *argv[])
     mytest[4] = Image("/home/student/1905008/IP/faces94/female/kaknig/kaknig.11.jpg");
 
 
+
+    //Set display 1 to matching training face
+    min_face_i = eigenface_match(mytest, IMAGE_COUNT);
+    m = mytest[min_face_i].ToQImage();
+    w2.SetImage(m);
+
+    //Set display 2 to test face
+    m = mytest[IMAGE_COUNT-1].ToQImage();
+    w.SetImage(m);
+
+    //Show the images
+    w.show();
+    w2.show();
+
+    return a.exec();
+}
+
+int eigenface_match( Image faces[], const int IMAGE_COUNT )
+{
     //Allocate matrices to store images
-    Matrix * mat[IMAGE_COUNT];
+    Matrix * face_matrices[IMAGE_COUNT];
     Matrix * mean_adjusted[IMAGE_COUNT];
     Matrix * eigenfaces[IMAGE_COUNT];
     Matrix * weights[IMAGE_COUNT];
     for( int i = 0; i < IMAGE_COUNT; i++)
     {
-       mat[i] = new Matrix(mytest[i].Width() *mytest[i].Height(),1);
-       mat[i]->SetFrom(mytest[i]);
-       mean_adjusted[i] = new Matrix(mytest[i].Width() *mytest[i].Height(),1);
-       eigenfaces[i] = new Matrix(mytest[i].Width() *mytest[i].Height(),1);
-       weights[i] = new Matrix(IMAGE_COUNT,IMAGE_COUNT);
+        face_matrices[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
+        face_matrices[i]->SetFrom(faces[i]);
+        mean_adjusted[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
+        eigenfaces[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
+        weights[i] = new Matrix(IMAGE_COUNT,IMAGE_COUNT);
     }
 
     //Calculate the average face
-    Matrix mean_face(mytest[0].Width()*mytest[1].Height(),1);
+    Matrix mean_face(faces[0].Width()*faces[1].Height(),1);
     for(int i = 0; i < IMAGE_COUNT-1; i++)
     {
-        mean_face.Add(*mat[i]);
+        mean_face.Add(*face_matrices[i]);
     }
     mean_face.Divide(IMAGE_COUNT-1);
-    Matrix A(mytest[0].Width()*mytest[1].Height(),IMAGE_COUNT-1);
+    Matrix A(faces[0].Width()*faces[0].Height(),IMAGE_COUNT-1);
     for(int i = 0; i < IMAGE_COUNT; i++)
     {
-        mean_adjusted[i]->Add(*mat[i]);
+        mean_adjusted[i]->Add(*face_matrices[i]);
         mean_adjusted[i]->Subtract(mean_face);
         if(i < IMAGE_COUNT-1)
             A.SetColumn(*mean_adjusted[i], i);
@@ -56,7 +94,7 @@ int main(int argc, char *argv[])
     Matrix L = Matrix::Multiply(At,A);
     Matrix eig_vals(IMAGE_COUNT-1, 1);
     Matrix eig_vecs(IMAGE_COUNT-1, IMAGE_COUNT-1);
-    Matrix U(mytest[0].Width()*mytest[1].Height(), IMAGE_COUNT-1);
+    Matrix U(faces[0].Width()*faces[1].Height(), IMAGE_COUNT-1);
     L.eigen(eig_vecs, eig_vals);
     for( int i = 0; i < IMAGE_COUNT-1; i++)
     {
@@ -75,7 +113,6 @@ int main(int argc, char *argv[])
     {
         Matrix temp = Matrix::Multiply(Ut,*mean_adjusted[i]);
         weights[i]->SetColumn(temp,0);
-        weights[i]->Print();
     }
     //Find the face with the closest response
     double min_mag = 100000.0;
@@ -91,31 +128,14 @@ int main(int argc, char *argv[])
         }
     }
 
-    std::cout << "The closest matching face is: " << min_face_i << std::endl;
-    QImage m = mytest[min_face_i].ToQImage();
-    w2.SetImage(m);
-
-    m = mytest[IMAGE_COUNT-1].ToQImage();
-    w.SetImage(m);
-
-
-
-    //Display the image in mytest at index 0
-    /*
-    Image t = eigenfaces[0]->ToImage(mytest[0].Height(), mytest[0].Width());
-    m = t.ToQImage();
-    w.SetImage(m);
-    */
-
     //Free matrices
     for( int i = 0; i < IMAGE_COUNT; i++)
     {
-       delete mat[i];
+        delete face_matrices[i];
         delete mean_adjusted[i];
         delete eigenfaces[i];
         delete weights[i];
     }
 
-    return a.exec();
+    return min_face_i;
 }
-
