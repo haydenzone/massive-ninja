38a2412a74536b925e0cae7405a8c8a870c61d1e
Hayden Waisanen <haydenzone@gmail.com>
2013-04-22 22:01:11 -0600

    FaceRecApp comments complete

--- StandaloneEigenfaces/facerecognitionapp.cpp
+++ StandaloneEigenfaces/facerecognitionapp.cpp
@@ -99,30 +99,61 @@ bool FaceRecognitionApp::Menu_FacialRecognition_Eigenfaces(Image &image)
     return true;
 }
 
+/***************************************************************************//**
+ * @author Hayden Waisanen
+ *
+ * @par Description:
+ * Use eigenfaces to calculate the image in faces array from index 0 to
+ * IMAGE_COUNT-2 closest to faces[IMAGE_COUNT-1].
+ *
+ * Algorithm translated from:
+ *    http://blog.zabarauskas.com/eigenfaces-tutorial/
+ *
+ * @param[in] Image faces[] -- Testing face is at index IMAGE_COUNT-1
+ *                          -- All other images are training
+ * @param[in] const int IMAGE_COUNT -- number of images in faces[]
+ * @param[in,out] Image eigen_faces[] -- a sampling of eigenfaces to be returned
+ * @param[in] int e_count -- number of eigen_faces to return
+ *
+ * @returns int -- image with closest match to test image
+ *
+ ******************************************************************************/
 int eigenface_match( Image faces[], const int IMAGE_COUNT, Image eigen_faces[], int e_count )
 {
     //Allocate matrices to store images
-    Matrix * face_matrices[IMAGE_COUNT];
-    Matrix * mean_adjusted[IMAGE_COUNT];
-    Matrix * eigenfaces[IMAGE_COUNT];
-    Matrix * weights[IMAGE_COUNT];
+    Matrix * face_matrices[IMAGE_COUNT]; //Column vector conversions of training images
+    Matrix * mean_adjusted[IMAGE_COUNT]; //Training images less the mean face
+    Matrix * eigenfaces[IMAGE_COUNT];    //Calculated eigenfaces
+    Matrix * weights[IMAGE_COUNT];       //Matrix containing contributions of each eigenface
+                                         //to the current face
+    const int NUM_PIXELS = faces[0].Width() *faces[0].Height(); //Number of pixels in images
+    Matrix mean_face(NUM_PIXELS,1);      //Average face
+    Matrix A(NUM_PIXELS,IMAGE_COUNT-1);  //A matrix
+    Matrix eig_vals(IMAGE_COUNT-1, 1);              //Eigen values matrix
+    Matrix eig_vecs(IMAGE_COUNT-1, IMAGE_COUNT-1);  //Eigen vectors matrix
+    Matrix U(NUM_PIXELS, IMAGE_COUNT-1); //U matrix
+
+    //Dynamically allocate the matrices
     for( int i = 0; i < IMAGE_COUNT; i++)
     {
-        face_matrices[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
-        face_matrices[i]->SetFrom(faces[i]);
-        mean_adjusted[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
-        eigenfaces[i] = new Matrix(faces[i].Width() *faces[i].Height(),1);
+        //Allocate
+        face_matrices[i] = new Matrix(NUM_PIXELS,1);
+        mean_adjusted[i] = new Matrix(NUM_PIXELS,1);
+        eigenfaces[i] = new Matrix(NUM_PIXELS,1);
         weights[i] = new Matrix((IMAGE_COUNT-1),1);
+
+        //Set face matrices to inputted images (including test image)
+        face_matrices[i]->SetFrom(faces[i]);
     }
 
     //Calculate the average face
-    Matrix mean_face(faces[0].Width()*faces[1].Height(),1);
     for(int i = 0; i < IMAGE_COUNT-1; i++)
     {
         mean_face.Add(*face_matrices[i]);
     }
     mean_face.Divide(IMAGE_COUNT-1);
-    Matrix A(faces[0].Width()*faces[0].Height(),IMAGE_COUNT-1);
+
+    //Calculate A matrix
     for(int i = 0; i < IMAGE_COUNT; i++)
     {
         mean_adjusted[i]->Add(*face_matrices[i]);
@@ -131,26 +162,36 @@ int eigenface_match( Image faces[], const int IMAGE_COUNT, Image eigen_faces[], 
             A.SetColumn(*mean_adjusted[i], i);
     }
 
+    //Calculate L = At * A
     Matrix At(Matrix::Transpose(A));
     Matrix L = Matrix::Multiply(At,A);
-    Matrix eig_vals(IMAGE_COUNT-1, 1);
-    Matrix eig_vecs(IMAGE_COUNT-1, IMAGE_COUNT-1);
-    Matrix U(faces[0].Width()*faces[1].Height(), IMAGE_COUNT-1);
+
+    //Find the eigenvalues and vectors
     L.eigen(eig_vecs, eig_vals);
+
+    //Create eigenfaces (U matrix)
     for( int i = 0; i < IMAGE_COUNT-1; i++)
     {
+        //Eigen values for C = A * At can be calculated  by
+        //multiplying eigenvectors from L by A
         Matrix temp(eig_vecs.GetCol(i));
         Matrix temp2(Matrix::Multiply(A, temp));
+
+        //Store and normalize eigenfaces
         eigenfaces[i]->SetColumn(temp2 ,0);
         eigenfaces[i]->Normalize();
         U.SetColumn(*eigenfaces[i], i);
 
-        //Scale eigne faces to display
-        eigenfaces[i]->Scale();
-        eigenfaces[i]->Multiply(255.0);
+        //Scale e_count number of eigenfaces and convert to an image
         if( i < e_count)
+        {
+            eigenfaces[i]->Scale();
+            eigenfaces[i]->Multiply(255.0);
             eigen_faces[i] = eigenfaces[i]->ToImage(faces[0].Height(),faces[0].Width());
+        }
     }
+
+    //Create transpose of U matrix
     Matrix Ut(Matrix::Transpose(U));
 
     //Calculate weights for each input image (including test)
@@ -159,7 +200,10 @@ int eigenface_match( Image faces[], const int IMAGE_COUNT, Image eigen_faces[], 
         Matrix temp = Matrix::Multiply(Ut,*mean_adjusted[i]);
         weights[i]->SetColumn(temp,0);
     }
+
     //Find the face with the closest response
+    //(considering weights[i] to be a point in "facespace",
+    //find the closest point, this is the training image with the closest match)
     double min_mag = DBL_MAX;
     int min_face_i = -1;
     for(int i = 0; i < IMAGE_COUNT-1; i++)
@@ -189,7 +233,8 @@ int eigenface_match( Image faces[], const int IMAGE_COUNT, Image eigen_faces[], 
  *
  * @par Description:
  * Get files contained in directory
- * Source: http://stackoverflow.com/questions/612097/how-can-i-get-a-list-of-files-in-a-directory-using-c-or-c
+ * Source:
+ * http://stackoverflow.com/questions/612097/how-can-i-get-a-list-of-files-in-a-directory-using-c-or-c
  *
  * @param[in] image
  *
@@ -204,6 +249,7 @@ bool get_files_dir(std::string dir_str, strvec & image_files)
     {
         while ((ent = readdir (dir)) != NULL)
         {
+            //Ignore hidden files
             if(ent->d_name[0] == '.')
                 continue;
             image_files.push_back(ent->d_name);
